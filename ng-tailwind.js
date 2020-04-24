6d6888fc2c4ac490bbf6fc1b091d2743e161ec59
Arief R Ramadhan <arief.rizky@doku.com>
2020-04-25 00:01:47 +0700

    Comment out bootstrap, setup Taildwindcss, update donation page

---
+++ ng-tailwind.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // Tailwind Paths
+  configJS: 'tailwind.config.js',
+  sourceCSS: 'src/main/webapp/content/css/tailwind.css',
+  outputCSS: 'src/main/webapp/content/css/styles.css',
+  // Sass
+  sass: false,
+  // PurgeCSS Settings
+  purge: false,
+  keyframes: false,
+  fontFace: false,
+  rejected: false,
+  whitelist: [],
+  whitelistPatterns: [],
+  whitelistPatternsChildren: [],
+  extensions: [
+    '.ts',
+    '.html',
+    '.js'
+  ],
+  extractors: [],
+  content: []
+}
