dfaa040783fb291ea60755d9fee62f8e1d00222c
Rudy Thong <rudy.thong@gmail.com>
2020-04-26 03:55:54 +0700

    Create config for MIB url to differentiate development and production. Development go to /api/payments-simulator

--- src/main/java/men/doku/donation/config/ApplicationProperties.java
+++ src/main/java/men/doku/donation/config/ApplicationProperties.java
@@ -5,9 +5,20 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 /**
  * Properties specific to Donation.
  * <p>
- * Properties are configured in the {@code application.yml} file.
- * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ * Properties are configured in the {@code application.yml} file. See
+ * {@link io.github.jhipster.config.JHipsterProperties} for a good example.
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+
+    private String mibUrl = new String();
+
+    public String getMibUrl() {
+        return mibUrl;
+    }
+
+    public void setMibUrl(String mibUrl) {
+        this.mibUrl = mibUrl;
+    }
+
 }
