ae1afdf1383ab7ae65c253836f8ed469d7c44a12
Arief R Ramadhan <arief.rizky@doku.com>
2020-04-29 00:03:15 +0700

    Remove bootstrap dependecies on dashboard

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { LoginModalService } from 'app/core/login/login-modal.service';
+import { LoginService } from 'app/core/login/login.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
 
@@ -13,8 +15,15 @@ import { Account } from 'app/core/user/account.model';
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   authSubscription?: Subscription;
+  isNavbarCollapsed = false;
+  isDropdownCollapsed = false;
 
-  constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
+  constructor(
+    private accountService: AccountService,
+    private loginModalService: LoginModalService,
+    private loginService: LoginService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
@@ -25,7 +34,30 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   login(): void {
+    this.collapseNavbar();
     this.loginModalService.open();
+  }
+
+  logout(): void {
+    this.collapseNavbar();
+    this.loginService.logout();
+    this.router.navigate(['']);
+  }
+
+  collapseNavbar(): void {
+    this.isNavbarCollapsed = true;
+  }
+
+  collapseDropdown(): void {
+    this.isDropdownCollapsed = true;
+  }
+
+  toggleNavbar(): void {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  toggleDropdown(): void {
+    this.isDropdownCollapsed = !this.isDropdownCollapsed;
   }
 
   ngOnDestroy(): void {
