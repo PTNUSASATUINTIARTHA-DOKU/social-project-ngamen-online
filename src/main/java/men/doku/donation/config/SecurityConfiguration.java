fba51e004effc4c0d8df09b1c0f1da089265c8b1
Rudy Thong <rudy.thong@gmail.com>
2020-05-20 12:29:27 +0700

    Update Content Security Policy in Spring Boot + Upgrade AWS from t2-micro into t3-small.

--- src/main/java/men/doku/donation/config/SecurityConfiguration.java
+++ src/main/java/men/doku/donation/config/SecurityConfiguration.java
@@ -64,7 +64,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .accessDeniedHandler(problemSupport)
         .and()
             .headers()
-            .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:")
+            .contentSecurityPolicy("default-src 'self';"
+                + " frame-src 'self' data:;"
+                + " script-src 'self' 'unsafe-inline' 'unsafe-eval'" 
+                + " https://www.googletagmanager.com/gtm.js https://www.google.com/recaptcha/api.js"
+                + " https://www.gstatic.com/;"
+                + " style-src 'self' 'unsafe-inline';"
+                + " img-src 'self' data: https:;"
+                + " font-src 'self' data: https:;"
+                + " object-src 'none';"
+                + " require-trusted-types-for 'script';"
+                + " base-uri 'self';")
         .and()
             .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
         .and()
